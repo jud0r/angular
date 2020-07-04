07554fd7dcc6270efc966094bcf3f56b314f1eea
Thiago Silva <thiago.silva@outlook.com>
2020-07-04 07:29:03 -0300

    Segregando responsabilidades: requisicoes http, pastas shared e core

---
+++ src/app/courses/course-module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CourseListComponent } from './course-list.component';
+import { CourseInfoComponent } from './course-info.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { StarModule } from '../shared/component/star/star.module'
+
+@NgModule({
+    declarations: [
+        CourseListComponent,
+        CourseInfoComponent,
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        StarModule,
+        RouterModule.forChild([
+            {
+                path: 'courses', component: CourseListComponent
+            },
+            {
+                path: 'courses/info/:id', component: CourseInfoComponent
+            }
+        ])
+    ]
+})
+export class CourseModule {
+
+}
